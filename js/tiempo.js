263b7270133609e2c04bcf5992f17e064f9c038d
Marlon Rosillo <marlon@mercasist.com>
2018-06-15 13:27:37 -0400

    Configuracion del JQuery

---
+++ js/tiempo.js
@@ -0,0 +1,9 @@
+$(function () {
+            $('#datetimepicker8').datetimepicker({
+                icons: {
+                    date: "fa fa-calendar",
+                    up: "fa fa-arrow-up",
+                    down: "fa fa-arrow-down"
+                }
+            });
+        });
