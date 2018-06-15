87ec051c411326a107049492bd266b198fa28f93
Marlon Rosillo <marlon@mercasist.com>
2018-06-15 15:48:50 -0400

    Cambios CSS

--- gestion/contenido_reporte.php
+++ gestion/contenido_reporte.php
@@ -81,7 +81,7 @@ echo "<div class='container centro'>";
                     //Cantidad del Artículo (Fin)
 
                     //Imagen del Artículo (Inicio)
-                    echo "<div class='imgArticulo'>";
+                    echo "<div class='imgArticulo' height='50' width='50'>";
                         echo "<img src=".$articulos[$k]['img_url'].">";
                     echo "</div>";
                     //Imagen del Artículo (Fin)
