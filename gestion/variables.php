67106e041bb092371aa83d5bb1d79b4fb7766c0d
Victor Guzman <vmgafrm@gmail.com>
2018-06-15 14:10:08 -0400

    creando variables

--- gestion/variables.php
+++ gestion/variables.php
@@ -1,7 +1,26 @@
 <?php
+	
 	include_once "../lib/clases.php";
 
-
-    $orden = 16718756;
+	$util=new Util;
+	$id_orden=$util->obtenerIdOrdenDeHash($hash);	
+	
+		
+	$orden=new Orden($id_orden);
+	$vendedor=new Usuario($orden->id_usuario);
+	
+	
+	
+	//VARIABLES DE ORDEN
+	$orden_id=$orden->id;
+	
+	$orden_fecha=aaaammdd_ddmmaaaa($orden->fecha);
+	
+	$vendedor_nombre=$vendedor->nombre;
+	$vendedor_nick=$vendedor->nick;
+	
+	
+	
+	
 
 ?>
